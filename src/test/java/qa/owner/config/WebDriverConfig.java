f0f93a626b3609d7b27a4817c400325a6df2fc42
Nikita Maniakin <scksckscksckscksck@gmail.com>
2023-04-19 00:18:19 +0300

    NEW CONFIG ADDED

--- src/test/java/qa/owner/config/WebDriverConfig.java
+++ src/test/java/qa/owner/config/WebDriverConfig.java
@@ -6,10 +6,13 @@ import java.net.URL;
 
 public interface WebDriverConfig extends Config {
     @Key("baseUrl")
+    @DefaultValue("https://github.com")
     String getBaseUrl();
     @Key("browser")
+    @DefaultValue("CHROME")
     Browser getBrowser();
 
     @Key("remoteUrl")
+    @DefaultValue("http://localhost:4444")
     URL getRemoteUrl();
 }
